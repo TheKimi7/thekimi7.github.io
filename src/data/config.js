bd6572a05b4b3fc76a8715e496d9ba1e3c95a418
TheKimi7 <ece20145@hotmail.com>
2026-04-20 02:00:27 +0530

    polished contactMe page with custom mail ID

--- src/data/config.js
+++ src/data/config.js
@@ -9,5 +9,5 @@ export const socials = [
   { icon: 'github', href: 'https://github.com/TheKimi7', label: 'GitHub' },
   { icon: 'linkedin', href: 'https://www.linkedin.com/in/pranav-rajendran/', label: 'LinkedIn' },
   { icon: 'x', href: 'https://x.com/thekimi_7', label: 'X' },
-  { icon: 'email', href: 'mailto:thekimi7.dev@gmail.com', label: 'Email' },
+  { icon: 'email', href: '/#contact-me', label: 'Email' },
 ]
